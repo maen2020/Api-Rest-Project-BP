67d6393fcf5a9178e7b345f5da11c6854c84df4f
maen210212 <maen210212@gmail.com>
2023-09-24 18:37:24 -0600

    I apply persistence to the entity.

--- src/main/java/com/maen/ec/ApiRestProject/model/entity/Customer.java
+++ src/main/java/com/maen/ec/ApiRestProject/model/entity/Customer.java
@@ -1,5 +1,6 @@
 package com.maen.ec.ApiRestProject.model.entity;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,12 +13,25 @@ import java.util.Date;
 @NoArgsConstructor //Constructor vacio.
 @AllArgsConstructor //Contructor con parametros.
 @ToString //Devuelve una representación en cadena de texto del objeto sobre el cual se invoque.
+@Entity //Definir la clase como entidad.
+@Table(name = "customers") //Hace referencia hacia la tabla de la base de datos.
 public class Customer {
 
     //Atributos.
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_customer")
     private Long idCustomer;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "lastname")
     private String lastname;
+
+    @Column(name = "email")
     private String email;
+
+    @Column(name = "registration_date")
     private Date registrationDate;
 }
