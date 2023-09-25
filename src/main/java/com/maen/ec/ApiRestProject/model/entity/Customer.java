177593c7d4c4262dbb6bdc541bfef3eecedbe5cf
maen210212 <maen210212@gmail.com>
2023-09-24 18:28:43 -0600

    Creation of the entity customers.

---
+++ src/main/java/com/maen/ec/ApiRestProject/model/entity/Customer.java
@@ -0,0 +1,14 @@
+package com.maen.ec.ApiRestProject.model.entity;
+
+import java.util.Date;
+
+//Esta clase hace referencia a una entidad o tabla de la base de datos.
+public class Customer {
+
+    //Atributos.
+    private Long idCustomer;
+    private String name;
+    private String lastname;
+    private String email;
+    private Date registrationDate;
+}
