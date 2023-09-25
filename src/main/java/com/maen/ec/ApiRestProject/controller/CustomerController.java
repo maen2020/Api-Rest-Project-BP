4bca1e3bc254f329135a0941f6857cfbf623953c
maen210212 <maen210212@gmail.com>
2023-09-24 20:41:21 -0600

    Applying Http Status Code | Response Status.

--- src/main/java/com/maen/ec/ApiRestProject/controller/CustomerController.java
+++ src/main/java/com/maen/ec/ApiRestProject/controller/CustomerController.java
@@ -3,6 +3,7 @@ package com.maen.ec.ApiRestProject.controller;
 import com.maen.ec.ApiRestProject.model.entity.Customer;
 import com.maen.ec.ApiRestProject.service.ICustomer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController //Indica que esta clase es un controlador.
@@ -14,22 +15,26 @@ public class CustomerController {
     private ICustomer customerService;
 
     @PostMapping("/customers")
+    @ResponseStatus(HttpStatus.CREATED)
     public Customer create(@RequestBody Customer customer){
         return customerService.save(customer);
     }
 
     @PutMapping("/customers")
+    @ResponseStatus(HttpStatus.CREATED)
     public Customer update(@RequestBody Customer customer){
         return customerService.save(customer);
     }
 
     @DeleteMapping("/customers/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable Long id){
         Customer customerDelete = customerService.findById(id);
         customerService.delete(customerDelete);
     }
 
     @GetMapping("/customers/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public Customer showById(@PathVariable Long id){
         return  customerService.findById(id);
     }
