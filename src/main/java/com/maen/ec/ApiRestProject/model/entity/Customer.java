68ca6828c6b318775b87e1fc912d076cc7e2f4ca
maen210212 <maen210212@gmail.com>
2023-09-24 18:30:52 -0600

    Application of lombok to the entity.

--- src/main/java/com/maen/ec/ApiRestProject/model/entity/Customer.java
+++ src/main/java/com/maen/ec/ApiRestProject/model/entity/Customer.java
@@ -1,8 +1,17 @@
 package com.maen.ec.ApiRestProject.model.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
 import java.util.Date;
 
 //Esta clase hace referencia a una entidad o tabla de la base de datos.
+@Data //Generar los metodos Get y Set en automatico.
+@NoArgsConstructor //Constructor vacio.
+@AllArgsConstructor //Contructor con parametros.
+@ToString //Devuelve una representación en cadena de texto del objeto sobre el cual se invoque.
 public class Customer {
 
     //Atributos.
