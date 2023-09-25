688153ccb1fe633d270aa7819629b37bb3c8a1b6
maen210212 <maen210212@gmail.com>
2023-09-24 18:15:24 -0600

    Started of the project.

---
+++ src/main/java/com/maen/ec/ApiRestProject/ApiRestProjectApplication.java
@@ -0,0 +1,12 @@
+package com.maen.ec.ApiRestProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestProjectApplication.class, args);
+	}
+}
---
+++ src/test/java/com/maen/ec/ApiRestProject/ApiRestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maen.ec.ApiRestProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
