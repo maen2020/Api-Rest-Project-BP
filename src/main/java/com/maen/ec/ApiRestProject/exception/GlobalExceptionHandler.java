1098bd614c0ac74c1cbd0f2e0690f776fa067525
Miguel Angel <miguelangel@192.168.1.8>
2023-09-27 18:04:08 -0600

    Applying @ExceptionHandler -BadRequestException.

--- src/main/java/com/maen/ec/ApiRestProject/exception/GlobalExceptionHandler.java
+++ src/main/java/com/maen/ec/ApiRestProject/exception/GlobalExceptionHandler.java
@@ -39,4 +39,11 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(apiResponse, HttpStatus.NOT_FOUND);
     }
 
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<ApiResponse> handlerBadRequestException(BadRequestException exception,
+                                                                WebRequest webRequest){
+        ApiResponse apiResponse = new ApiResponse(exception.getMessage(), webRequest.getDescription(false));
+        return new ResponseEntity<>(apiResponse, HttpStatus.BAD_REQUEST);
+    }
+
 }
